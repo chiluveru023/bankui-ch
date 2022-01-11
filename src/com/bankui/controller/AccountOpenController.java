5d39714a410df46463d0dd4a112c59490f575221
Raji <chiluveru023@gmail.com>
2022-01-11 13:15:26 +0530

    login added

--- src/com/bankui/controller/AccountOpenController.java
+++ src/com/bankui/controller/AccountOpenController.java
@@ -5,6 +5,11 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.client.RestTemplate;
+import com.bankui.beans.UserInfo;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
 
 import com.bankui.beans.Customer;
 @Controller
@@ -12,19 +17,57 @@ public class AccountOpenController {
 	@Autowired
 	private Customer customer;
 	
+	@Autowired
+	private UserInfo user;
+	
 @RequestMapping("/")
 	public String showAccountOpenScreen(Model model) {
 	model.addAttribute("customer",customer);
 	return "account_open";
 	}
 @RequestMapping("/create-account")
-   public String showLoginScreen(@ModelAttribute Customer customer) {
-	String url="http://localhost:8585/customer";
+   public String showLoginScreen(@ModelAttribute Customer customer,Model model) {
 	//Call the API and pass this customer object 
+	String url="http://localhost:8585/customer";
+	
 			
 			RestTemplate restTemplate = new RestTemplate(); 
 			Customer c = restTemplate.postForObject(url, customer, Customer.class);
+			model.addAttribute("user", user);
+			model.addAttribute("msg", "");
 			return "login";
 	
+}
+@RequestMapping("/process-login")
+public String processLogin(@ModelAttribute UserInfo user, Model model) {
+	//generate encoded auth_code using username and password and attach it to headers
+	HttpHeaders headers = new HttpHeaders();
+	headers.setBasicAuth(user.getUsername(), user.getPassword());
+
+	//attach this header to request Object 
+	HttpEntity<Object> request = new HttpEntity<>(headers); 
+
+	//call the api
+	RestTemplate restTemplate = new RestTemplate();
+	String url="http://localhost:8585/user";
+
+	try {
+		ResponseEntity<UserInfo> entity = restTemplate.exchange(url, HttpMethod.GET, request, UserInfo.class);
+		UserInfo u = entity.getBody();
+		model.addAttribute("user", u);
+		return "dashboard";
+	}
+	catch(Exception e) {
+		model.addAttribute("user", user);
+		model.addAttribute("msg", "Invalid Credentials"); 
+		return "login";
+	}
+
+}
+@RequestMapping("/login")
+public String showLogin(Model model) {
+	model.addAttribute("user", user);
+	model.addAttribute("msg", ""); 
+	return "login";
 }
 }
