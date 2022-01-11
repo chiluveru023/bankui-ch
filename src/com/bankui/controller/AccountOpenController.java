fa805bb9159a43bd30622a8598d244422e58f2a1
Raji <chiluveru023@gmail.com>
2022-01-11 05:55:41 +0530

    model added

--- src/com/bankui/controller/AccountOpenController.java
+++ src/com/bankui/controller/AccountOpenController.java
@@ -1,10 +1,30 @@
 package com.bankui.controller;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.client.RestTemplate;
+
+import com.bankui.beans.Customer;
 @Controller
 public class AccountOpenController {
+	@Autowired
+	private Customer customer;
+	
 @RequestMapping("/")
-	public String showAccountOpenScreen() {
-		return "account_open";
+	public String showAccountOpenScreen(Model model) {
+	model.addAttribute("customer",customer);
+	return "account_open";
 	}
+@RequestMapping("/create-account")
+   public String showLoginScreen(@ModelAttribute Customer customer) {
+	String url="http://localhost:8585/customer";
+	//Call the API and pass this customer object 
+			
+			RestTemplate restTemplate = new RestTemplate(); 
+			Customer c = restTemplate.postForObject(url, customer, Customer.class);
+			return "login";
+	
+}
 }
